b689ffb23fe5d55c0b17a1ac40ec2b39909e6d81
Yasir Akhlaque <akhlaqueyasir28@gmail.com>
2024-07-31 20:22:26 +0530

    removed unnecessary files

--- Checkbox/scripts.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('checkbox').addEventListener('change', function() {
-    const explodeAnimation = document.querySelector('.explode-animation');
-    explodeAnimation.style.animation = 'none';
-    setTimeout(() => {
-        explodeAnimation.style.animation = '';
-    }, 10);
-});
--- Image Carousel/script.js
+++
@@ -1,6 +0,0 @@
-let slide = document.querySelectorAll('slide');
-let pic1 = document.querySelector('pic1');
-let pic2 = document.querySelector('pic2');
-let pic3 = document.querySelector('pic3');
-
-
