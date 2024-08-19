f25713eff8dd5f504c7a98c72350454831973ae9
Yasir Akhlaque <akhlaqueyasir28@gmail.com>
2024-08-19 14:53:45 +0530

    Updated Slider Section

---
+++ Movies/script.js
@@ -0,0 +1,15 @@
+var idxVal = 0;
+function Slideshow() {
+    setTimeout(Slideshow,2500);
+    var x;
+    const img = document.querySelectorAll('.images img');
+    for(x=0;x<img.length ; x++){
+        img[x].style.display='none';
+    }
+    idxVal++;
+    if(idxVal > img.length){
+        idxVal =1;
+    }
+    img[idxVal-1].style.display='block';
+}
+Slideshow();
